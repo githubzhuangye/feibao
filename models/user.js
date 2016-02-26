07c3125b92f035c009e9ce340b4bdfd957bb8c8c
githubzhuangye <xinlangzhuangye@sina.com>
2016-02-26 14:09:37 +0800

    add waste_sort

---
+++ models/user.js
@@ -0,0 +1,8 @@
+var mongoose =require('mongoose');
+module.exports =mongoose.model('User',new mongoose.Schema({
+        name:String,
+        password:String,
+        phone:Number,
+        address:String,
+        balance:String
+}));
